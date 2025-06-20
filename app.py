31a46233379ddd95cf4dc4c48050a5cff7b5951d
ofcRealXT <talhaozc.2010@gmail.com>
2025-06-20 17:19:38 +0300

    HTML sayfaları hataya bağlandı

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template("errors/403.html"), 403
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("errors/500.html"), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
